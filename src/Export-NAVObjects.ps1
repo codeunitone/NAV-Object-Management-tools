8d1e210604cb2512b0e936087b019632d4c82b70
Benjamin Alisch <benjamin.alisch@misterspex.de>
2018-07-02 16:46:14 +0200

    Adding help for Export-NAVObjects.ps1

--- src/Export-NAVObjects.ps1
+++ src/Export-NAVObjects.ps1
@@ -1,3 +1,44 @@
+<#
+.SYNOPSIS
+
+This script export Objects as TXT.
+
+.Description
+
+Exports objects from a NAV database as TXT into a givin target folder. The script creates subfolders for each object type inside the target folder.
+
+.PARAMETER dbServerName
+
+Server name where the NAV database is located
+
+.PARAMETER dbName
+
+Name of the NAV database
+
+.PARAMETER targetFolder
+
+The exported objects will be stored at this location
+
+.PARAMETER tempExportPath
+
+Filtered Objects will be exported into one big txt file and afterwards splited and moved into Subfolders. This parameter defines the location of that export file.
+
+.PARAMETER objFilterList
+
+Text Array of Objects to export. 
+
+.EXAMPLE
+
+Following example exports from the database NAV2018-DEV (located on GWS-NAV-2018QA) all tables within the range of 50000 till 50005 and all pages within the range of 40 and 50.
+
+.\src\Export-NAVObjects.ps1 -dbServerName "GWS-NAV-2018QA" -dbName "NAV2018-DEV" -targetFolder "C:\Temp\NAV-6694\split\" -objFilterList "Type=Table;Id=50000..50005","Type=Page;Id=40..50"
+
+.NOTES
+
+You have to have NAV 2018 Development Shell installed
+
+#>
+
 param (
 	[Parameter(Mandatory=$true)] [string] $dbServerName,
 	[Parameter(Mandatory=$true)] [string] $dbName,
@@ -5,11 +46,6 @@ param (
 	[Parameter(Mandatory=$true)] [string] $tempExportPath,
 	[Parameter(Mandatory=$true)] [string[]] $objFilterList
 )
-
-# About
-# - script creates subfolder in $targetFolder for each object type
-# - You have to have NAV 2018 Development Shell installed
-
 
 &'C:\Program Files (x86)\Microsoft Dynamics NAV\110\RoleTailored Client\NavModelTools.ps1'
 
